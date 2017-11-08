2a39f32ab47466bb89d3911dc6255e491c07e519
Anh mai <lyraducusin@gmail.com>
2017-11-07 21:19:36 -0500

    update

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
 $(document).ready(function() {
 
-  $(".available").click(function () {
-      $(".form").css("display","block");
-      $(".tableNumber").text("Table Number: " + this.innerHTML);
-      $(".available").css("z-index","-20");
+var available;
+var name;
+var container;
+var userText;
 
-    $(".save").click(function(){
-        $(".form").css("display","none");
-        $(".available").css("z-index","0");
-      })
+  $(".available").click(function () {
+    $("form").toggle(true);
+    $(".tableNumber").text("Table Number: " + this.innerHTML);
+    container = $(this);
   });
 
-  $(".available").mouseenter(function () {
-    $(this).fadeTo(400, .25);
-    });
-    $(".available").mouseleave(function () {
-    $(this).fadeTo(400, 1);
-    });
+  $(".save").on("click",function(event){
+    $("form").css("display","none");
+    $(".available").css("z-index","0");
+    var userName = $(".name").val();
+    var userNum = $(".phoneNumber").val();
+    var userGuestNum = $(".numberOfParty").val();
+    console.log(userName, userNum, userGuestNum);
+    container.attr("name", userName);
+    container.addClass("reserved");
+    container.removeClass("available");
+    $("form").hide();
+    $("tableNumber").remove(this.innerHTML);
+    container.off("click");
+    $(".reserved").append("<div class='test'>" + userName + userNum + userGuestNum + "</div>")
 
+  });
+
+  // $(".reserved").on("mouseenter", function(event) {
+  //   //  $(this).append(""<div class="enter">"" + userName "</div>")
+  //   $(this).text(name);
+  // }).on("mouseleave", function(event) {
+  //   $(this).text(userText);
+  // });
+
+  $(".available").on("mouseenter", function(event) {
+    if($(this).hasClass("reserved")){
+      name = $(this).attr("name");
+      userGuestNum = $(this).attr("numberOfParty");
+      userText = $(this).text();
+
+      $(this).text("");
+      $(this).append("<div class ='hoverBox'>" + "Name: " + name + "Party size: "  + userGuestNum + "</div>");
+      // $(this).text.append("<div>" + name + userGuestNum "</div>")
+    }
+
+  }).on("mouseleave", function(event) {
+    if($(this).hasClass("reserved")){
+      $(this).text(userText);
+    }
+
+  });
+
+  $(".boxclose").on("click",function(){
+    $("form").toggle(false);
+  });
 });
